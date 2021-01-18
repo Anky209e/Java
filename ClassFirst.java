b7463e55ced49390e9939792eeb3d30148fd780f
Ankit Kumar <71585377+Anky209e@users.noreply.github.com>
2021-01-18 12:20:38 +0530

    Creating our First Java class.
    We came to know about structure of a java programme.

---
+++ ClassFirst.java
@@ -0,0 +1,12 @@
+public class ClassFirst {     // PascalMethod
+    public static void main(String[]args) //camelCase
+    {
+        System.out.println("Hello world");
+        int num1 =2;
+        int num2 = 4;
+        int sum = num1+num2;
+        System.out.println("This is your answer");   //here ln is used with print to give a new line
+        System.out.println(sum);
+    }
+    
+}
