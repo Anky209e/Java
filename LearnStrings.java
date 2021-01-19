9cd1f389ab706a4d18a44b62184cf86a53d6fbd0
Ankit Kumar <71585377+Anky209e@users.noreply.github.com>
2021-01-19 19:06:14 +0530

    Learn strings basic

---
+++ LearnStrings.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class LearnStrings {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        // String name = new String("ankit");  // we can use string as both class and primitive datatype
+        // String name = "java";
+        // System.out.print("The name is ");
+        // System.out.println(name);
+        Scanner sc = new Scanner(System.in);
+        String st = sc.nextLine();
+        System.out.println(st);
+
+
+
+
+
+
+
+
+        sc.close();
+
+
+    }
+}
