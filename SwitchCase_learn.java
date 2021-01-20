b040e174ce6c2e476f2570faa6e2d6b06688927e
Ankit Kumar <71585377+Anky209e@users.noreply.github.com>
2021-01-20 17:47:23 +0530

    Learning switch case statements in java

---
+++ SwitchCase_learn.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class SwitchCase_learn {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name = sc.nextLine();
+        switch(name){
+            case "ankit":
+                System.out.println("Hey! you litttle");
+                break;
+            case "capti":
+                System.out.println("You are a pussy man");
+                break;
+            case "silver":
+                System.out.println("Oh you gourav");
+                break;
+            default:
+                System.out.println("Wait a minute who are you?");
+
+
+
+
+        }
+
+        sc.close();
+        
+        }
+
+
+
+
+
+
+
+    }
+    
+
