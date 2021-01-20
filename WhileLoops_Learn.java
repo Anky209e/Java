b1f4f8e72f4c357d3e18771c8e06b6aa80e83144
Ankit Kumar <71585377+Anky209e@users.noreply.github.com>
2021-01-20 20:37:06 +0530

    Learning while loops in java

---
+++ WhileLoops_Learn.java
@@ -0,0 +1,10 @@
+public class WhileLoops_Learn {
+    public static void main(String[] args) {
+        int a = 1;
+        while (a<=5) {
+            System.out.println(a);
+            a++;
+        }
+    }
+    
+}
