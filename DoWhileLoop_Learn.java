b8a4262ed68a112247861487b696e9f4111e6995
Ankit Kumar <71585377+Anky209e@users.noreply.github.com>
2021-01-20 21:24:57 +0530

    Learning do while loop in java

---
+++ DoWhileLoop_Learn.java
@@ -0,0 +1,12 @@
+public class DoWhileLoop_Learn {
+    public static void main(String[] args) {
+        // only diffrence between do while and while is that do while will run 1 time atleast withou checking condition
+
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i<=5);
+    }
+    
+}
