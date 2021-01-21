5410986247c168895167514f1419ba7753bbc08c
Ankit Kumar <71585377+Anky209e@users.noreply.github.com>
2021-01-21 14:11:24 +0530

    Practice of loops

---
+++ PracticeSet_four.java
@@ -0,0 +1,30 @@
+public class PracticeSet_four {
+    public static void main(String[] args) {
+        // int a = 4;
+        // for(int i = a;i>0;i--){
+        //     // System.out.println("*");
+        //     for(int j=0;j<i;j++){
+        //         System.out.print("*");
+        //     }
+        // System.out.print("\n");
+
+        // // }
+        // // int n = 3;
+        // for(int i=0;i<=10;i++){
+        //     System.out.printf("%d*%d=%d\n",n,i,n*i);
+        // }
+        // // for revrerse
+        // // int n = 3;
+        // for(int i=10;i>=0;i--){
+        //     System.out.printf("%d*%d=%d\n",n,i,n*i);
+        // }
+    // int i =1;
+    // int n = 5;
+    // while(n>=i){
+    //     System.out.println(i*n);
+    //     n--;
+    // }
+    
+    
+    }
+}
